759a241a7c8070fa27266d01c1f0d48530f9d5bf
Oleksii Zeleniuk <alexzelenuyk@gmail.com>
2019-01-25 17:35:58 +0100

    remove temp files

--- .build/parse/attachments.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const PDF_SUFFIX = '.pdf';
-function isPdfFile(pdf) {
-    return pdf.filename.endsWith(PDF_SUFFIX);
-}
-exports.isPdfFile = isPdfFile;
--- .build/persist/s3KeyPrefix.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getS3KeyPrefix() {
-    const date = new Date(Date.now());
-    return `${date.getFullYear()}/${date.getMonth() + 1}`;
-}
-exports.getS3KeyPrefix = getS3KeyPrefix;
